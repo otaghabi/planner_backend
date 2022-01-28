16c08281f5bb5b0b7c68ae5abcf31a4aaff8f0e9
Ali Bagheri <khodealib@gmail.com>
2022-01-28 12:48:57 +0330

    Clear global urls file.

--- config/urls.py
+++ config/urls.py
@@ -1,18 +1,3 @@
-"""config URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
@@ -35,14 +20,20 @@ schema_view = get_schema_view(
     permission_classes=(AllowAny,),
 )
 
+swagger_urlpatterns_debug = [
+    re_path(r'^doc/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-redoc')
+]
+
+swagger_urlpatterns_production = [
+    re_path(r'^doc/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-redoc')
+]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('api/v1/', include('api.urls')),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+urlpatterns += swagger_urlpatterns_production
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
