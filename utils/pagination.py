3e0e8e169866511941bd4f971747ab7c7bf8439c
Ali Bagheri <khodealib@gmail.com>
2022-02-04 00:43:24 +0330

    Add pagination param in response.py

---
+++ utils/pagination.py
@@ -0,0 +1,22 @@
+from rest_framework import pagination
+
+from utils.response import successful_response
+
+
+DEFAULT_PAGE_SIZE = 20
+
+
+class PlannerPagination(pagination.PageNumberPagination):
+    page_size = DEFAULT_PAGE_SIZE
+    page_size_query_param = 'page_size'
+
+    def get_paginated_response(self, data):
+        return successful_response(
+            data=data,
+            pagination={
+                'current_page': self.page.number,
+                'has_more_page': self.page.has_next(),
+                'per_page': self.page_size,
+                'total': self.page.paginator.count
+            }
+        )
--- utils/response.py
+++ utils/response.py
@@ -2,12 +2,15 @@ from rest_framework import status
 from rest_framework.response import Response
 
 
-def successful_response(data=None, status_code=status.HTTP_200_OK):
+def successful_response(data=None, pagination=None, status_code=status.HTTP_200_OK):
     content = {
         'success': True,
     }
     if data is not None:
         content['data'] = data
+
+    if pagination is not None:
+        content['pagination'] = pagination
 
     return Response(content, status=status_code)
 
