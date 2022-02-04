81034dc49aaaaacf0604a7ba106b2178712711c5
Ali Bagheri <khodealib@gmail.com>
2022-02-04 05:24:50 +0330

    Change DB to MySql from Sqlite.

--- config/settings.py
+++ config/settings.py
@@ -104,8 +104,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'planner_db',
+        'USER': 'root',
+        'PASSWORD': 'Alib@0371499720',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
