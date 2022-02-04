c7d4ad045f9d66b1888f46489c21c82190acc841
Ali Bagheri <khodealib@gmail.com>
2022-02-04 05:59:00 +0330

    Add sample.env file.

--- config/settings.py
+++ config/settings.py
@@ -1,17 +1,27 @@
+import os
 from datetime import timedelta
 from pathlib import Path
 
+# env setting
+from environ import Env, environ
+
+env = Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o7p#5s61^(x16gk517bc!63ki=se*quxu)f4i4jn51v@+oi0u-'
+SECRET_KEY = env('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
@@ -105,11 +115,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'planner_db',
-        'USER': 'root',
-        'PASSWORD': 'Alib@0371499720',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),   # Or an IP Address that your DB is hosted on
+        'PORT': env('DB_PORT'),
     }
 }
 
