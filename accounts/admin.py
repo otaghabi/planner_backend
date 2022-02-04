8d11e03067af7643190edb604b8e0d26ef468385
Ali Bagheri <khodealib@gmail.com>
2022-02-04 05:06:30 +0330

    Fix UserAdmin add form

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from accounts.models import User, Student, Advisor
 
 
 @admin.register(User)
-class UserAdmin(UserAdmin):
+class UserAdmin(BaseUserAdmin):
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal info', {'fields': (
@@ -19,8 +19,14 @@ class UserAdmin(UserAdmin):
         }),
         ('Important dates', {'fields': ('last_login', 'date_joined')}),
     )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
     list_filter = ('gender', 'is_staff', 'is_superuser', 'is_active', 'groups')
-    list_display = ('email', 'first_name', 'last_name', 'avatar', 'is_staff', 'is_active')
+    list_display = ('id', 'email', 'first_name', 'last_name', 'avatar', 'is_staff', 'is_active')
     ordering = ('email',)
     search_fields = ('email', 'first_name', 'last_name')
     list_editable = ('is_staff', 'is_active')
