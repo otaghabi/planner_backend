86885a0fcbe4ab7433022ff70153df70d4557b02
Ali Bagheri <khodealib@gmail.com>
2022-02-04 14:10:32 +0330

    Change contact email.

--- config/urls.py
+++ config/urls.py
@@ -13,7 +13,7 @@ schema_view = get_schema_view(
         default_version='v1',
         description="Planner API Document",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="plannerforuse@gmail.com"),
+        contact=openapi.Contact(email="khodealib@gmail.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
